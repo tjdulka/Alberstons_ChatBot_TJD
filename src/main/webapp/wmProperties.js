8687396cf77a4bb6783d4665a6635c708e997267
root <root@6b8a8f8f3348>
2017-04-14 18:09:15 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Alberstons-ChatBot-TJD",
+  "homePage" : "Main",
+  "name" : "Alberstons_ChatBot_TJD",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
