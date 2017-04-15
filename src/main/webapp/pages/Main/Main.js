570a778f2ffec1cbebb6602b2455576b0a562f18
Thomas Dulka <tjdulka@us.ibm.com>
2017-04-15 02:58:28 +0000

    all good New files added - [mybluemix] Files modified - [Main.variables.json,Main.js,project-services.xml,pom.xml,deployment.properties,development.properties]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -51,6 +51,21 @@ Application.$controller("MainPageController", ["$scope", function($scope) {
                 $scope.Variables.order_for_detail.dataSet = {
                     "dataValue": input.text
                 };
+            } else if (output_text[0].startsWith('(--OMS_GET_ORDERS_ALL--)')) {
+                output_text[0] = 'Here is a list of all recent orders'
+                $scope.Variables.htmlContent.dataSet = {
+                    "dataValue": ''
+                };
+                var counter = $scope.Variables.forceOrdersInvoke.dataSet.dataValue;
+                counter += 1;
+                $scope.Variables.forceOrdersInvoke.dataSet = {
+                    "dataValue": counter
+                };
+            } else {
+                $scope.Variables.htmlContent.dataSet = {
+                    "dataValue": ''
+                };
+
             }
 
         }
@@ -58,49 +73,94 @@ Application.$controller("MainPageController", ["$scope", function($scope) {
     };
 
     $scope.svcOrdersByProductonResult = function(variable, data) {
-        var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
-        msg += '<tr><td><b>Product:</b></td><td colspan="5">' + data.results.PRODUCT + '</td></tr>';
-        msg += '<tr><td><b>Order Number</b></td><td><b>Delivery Date</b></td><td><b>Description</b></td><td><b>Order Quantity</b></td><td><b>Modify Quantity</b></td><td><b>Ship Quantity</b></td></tr>';
-        data.results.ORDERS.forEach(function(order, index) {
-            msg += '<tr><td>' + order.ORDER_NBR + '</td><td>' + order.SCHEDULE_DELIVERY_DT + '</td><td>' + order.ITEM_DSC + '</td><td>' + order.ORDER_QTY + '</td><td>' + order.MODIFY_QTY + '</td><td>' + order.SHIP_QTY + '</td></tr>';
-        });
+        if (data.results.PRODUCT != '') {
+            var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
+            msg += '<tr><td><b>Product:</b></td><td colspan="5">' + data.results.PRODUCT + '</td></tr>';
+            msg += '<tr><td><b>Order Number</b></td><td><b>Delivery Date</b></td><td><b>Description</b></td><td><b>Order Quantity</b></td><td><b>Modify Quantity</b></td><td><b>Ship Quantity</b></td></tr>';
+            data.results.ORDERS.forEach(function(order, index) {
+                msg += '<tr><td>' + order.ORDER_NBR + '</td><td>' + order.SCHEDULE_DELIVERY_DT + '</td><td>' + order.ITEM_DSC + '</td><td>' + order.ORDER_QTY + '</td><td>' + order.MODIFY_QTY + '</td><td>' + order.SHIP_QTY + '</td></tr>';
+            });
 
-        msg += '</tbody></table>';
-        console.log('msg: ', msg);
-        $scope.Variables.htmlContent.dataSet = {
-            "dataValue": msg
+            msg += '</tbody></table>';
+            $scope.Variables.htmlContent.dataSet = {
+                "dataValue": msg
+            };
+            $scope.Variables.product.dataSet = {
+                "dataValue": ''
+            };
         };
     };
 
 
     $scope.svcOrderStatusonResult = function(variable, data) {
-        var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
-        msg += '<tr><td><b>Order Number:</b></td><td colspan="5">' + data.results.ORDER_NBR + '</td></tr>';
-        msg += '<tr><td><b>Order Date</b></td><td><b>Whse</b></td><td><b>Store</b></td><td><b>User ID</b></td><td><b>Delivery Date</b></td></tr>';
-        data.results.ORDER_STATUS.forEach(function(order, index) {
-            msg += '<tr><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td><td>' + order.DEST_FACILITY_ID + '</td><td>' + order.DW_CREATE_USER_ID + '</td><td>' + order.SCHEDULE_DELIVERY_DT + '</td></tr>';
-        });
+        if (data.results.ORDER_NBR != '') {
+            var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
+            msg += '<tr><td><b>Order Number:</b></td><td colspan="5">' + data.results.ORDER_NBR + '</td></tr>';
+            msg += '<tr><td><b>Order Date</b></td><td><b>Whse</b></td><td><b>Store</b></td><td><b>User ID</b></td><td><b>Delivery Date</b></td></tr>';
+            data.results.ORDER_STATUS.forEach(function(order, index) {
+                msg += '<tr><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td><td>' + order.DEST_FACILITY_ID + '</td><td>' + order.DW_CREATE_USER_ID + '</td><td>' + order.SCHEDULE_DELIVERY_DT + '</td></tr>';
+            });
 
-        msg += '</tbody></table>';
-        console.log('msg: ', msg);
-        $scope.Variables.htmlContent.dataSet = {
-            "dataValue": msg
+            msg += '</tbody></table>';
+            $scope.Variables.htmlContent.dataSet = {
+                "dataValue": msg
+            };
+            $scope.Variables.order_for_status.dataSet = {
+                "dataValue": ''
+            };
         };
+
     };
 
 
     $scope.svcOrderDetailonResult = function(variable, data) {
-        var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
-        msg += '<tr><td><b>Order Number:</b></td><td colspan="5">' + data.results.ORDER_NBR + '</td></tr>';
-        msg += '<tr><td><b>Item</b></td><td><b>Order Qty</b></td><td><b>Modify Qty</b></td><td><b>Ship Qty</b></td></tr>';
-        data.results.ORDER_DETAIL.forEach(function(order, index) {
-            msg += '<tr><td>' + order.ITEM_DSC + '</td><td>' + order.ORDER_QTY + '</td><td>' + order.MODIFY_QTY + '</td><td>' + order.SHIP_QTY + '</td></tr>';
-        });
+        if (data.results.ORDER_NBR != '') {
+            var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
+            msg += '<tr><td><b>Order Number:</b></td><td colspan="5">' + data.results.ORDER_NBR + '</td></tr>';
+            msg += '<tr><td><b>Item</b></td><td><b>Order Qty</b></td><td><b>Modify Qty</b></td><td><b>Ship Qty</b></td></tr>';
+            data.results.ORDER_DETAIL.forEach(function(order, index) {
+                msg += '<tr><td>' + order.ITEM_DSC + '</td><td>' + order.ORDER_QTY + '</td><td>' + order.MODIFY_QTY + '</td><td>' + order.SHIP_QTY + '</td></tr>';
+            });
 
-        msg += '</tbody></table>';
-        console.log('msg: ', msg);
-        $scope.Variables.htmlContent.dataSet = {
-            "dataValue": msg
+            msg += '</tbody></table>';
+            $scope.Variables.htmlContent.dataSet = {
+                "dataValue": msg
+            };
+            $scope.Variables.order_for_status.dataSet = {
+                "dataValue": ''
+            };
+        };
+    };
+
+
+    $scope.svcOrdersonResult = function(variable, data) {
+        /* 
+        var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
+         msg += '<tr><td><b>Store</b></td><td><b>Order Number</b></td><td><b>Order Date</b></td><td><b>Whse</b></td></tr>';
+         data.results.ORDERS.forEach(function(order, index) {
+             msg += '<tr><td>' + order.DEST_FACILITY_ID + '</td><td>' + order.ORDER_NBR + '</td><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td></tr>';
+         });
+
+         msg += '</tbody></table>';
+         $scope.Variables.htmlContent.dataSet = {
+             "dataValue": msg
+         };
+         */
+    };
+
+    $scope.mybluemixonResult = function(variable, data) {
+        if ($scope.Variables.forceOrdersInvoke.dataSet.dataValue > 0) {
+            console.log('counter: ', $scope.Variables.forceOrdersInvoke.dataSet.dataValue);
+            var msg = '<table style="text-align: left; " border="0" cellpadding="2" cellspacing="2"><tbody>';
+            msg += '<tr><td><b>Store</b></td><td><b>Order Number</b></td><td><b>Order Date</b></td><td><b>Whse</b></td></tr>';
+            data.results.ORDERS.forEach(function(order, index) {
+                msg += '<tr><td>' + order.DEST_FACILITY_ID + '</td><td>' + order.ORDER_NBR + '</td><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td></tr>';
+            });
+
+            msg += '</tbody></table>';
+            $scope.Variables.htmlContent.dataSet = {
+                "dataValue": msg
+            };
         };
     };
 
