7ca0631c22ae26705eb4b0da50210d947afef0e0
T.J. Dulka <tjdulka@us.ibm.com>
2017-04-17 22:38:07 +0000

    test variables New files added - [svcGetOrderExceptionsByProductAndStore,svcOrderExceptionsByStore,svcOrdersByProductAndStore] Files modified - [Main.variables.json,Main.html,Main.js,project-services.xml,pom.xml,deployment.properties,development.properties]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -9,6 +9,9 @@ Application.$controller("MainPageController", ["$scope", function($scope) {
         $scope.Widgets.OrdersByProductContainer.show = false;
         $scope.Widgets.OrdersByStoreContainer.show = false;
         $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
     };
 
     $scope.askWatsonWatsonresponse = function($isolateScope) {
@@ -85,50 +88,30 @@ Application.$controller("MainPageController", ["$scope", function($scope) {
 
     $scope.svcOrdersByProductonResult = function(variable, data) {
         if (data.results.PRODUCT != '') {
-            var msg = '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2"><tbody>';
-            msg += '<tr><td><b>Product:</b></td><td colspan="5">' + data.results.PRODUCT + '</td></tr>';
-            msg += '<tr><td><b>Order Number</b></td><td><b>Delivery Date</b></td><td><b>Description</b></td><td><b>Order Quantity</b></td><td><b>Modify Quantity</b></td><td><b>Ship Quantity</b></td></tr>';
-            data.results.ORDERS.forEach(function(order, index) {
-                msg += '<tr><td>' + order.ORDER_NBR + '</td><td>' + order.SCHEDULE_DELIVERY_DT + '</td><td>' + order.ITEM_DSC + '</td><td>' + order.ORDER_QTY + '</td><td>' + order.MODIFY_QTY + '</td><td>' + order.SHIP_QTY + '</td></tr>';
-            });
-            msg += '</tbody></table>';
-            $scope.Variables.htmlContent.dataSet = {
-                "dataValue": msg
-            };
-            $scope.Variables.product.dataSet = {
-                "dataValue": ''
-            };
             $scope.Widgets.OrdersContainer.show = false;
             $scope.Widgets.OrderStatusContainer.show = false;
             $scope.Widgets.OrderDetailContainer.show = false;
             $scope.Widgets.OrdersByProductContainer.show = true;
             $scope.Widgets.OrdersByStoreContainer.show = false;
             $scope.Widgets.OrdersByUserContainer.show = false;
+            $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+            $scope.Widgets.OEByStoreContainer.show = false;
+            $scope.Widgets.OEByProductAndStoreContainer.show = false;
         };
     };
 
 
     $scope.svcOrderStatusonResult = function(variable, data) {
         if (data.results.ORDER_NBR != '') {
-            var msg = '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2"><tbody>';
-            msg += '<tr><td><b>Order Number:</b></td><td colspan="4">' + data.results.ORDER_NBR + '</td></tr>';
-            msg += '<tr><td><b>Order Date</b></td><td><b>Whse</b></td><td><b>Store</b></td><td><b>User ID</b></td><td><b>Delivery Date</b></td></tr>';
-            data.results.ORDER_STATUS.forEach(function(order, index) {
-                msg += '<tr><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td><td>' + order.DEST_FACILITY_ID + '</td><td>' + order.DW_CREATE_USER_ID + '</td><td>' + order.SCHEDULE_DELIVERY_DT + '</td></tr>';
-            });
-            msg += '</tbody></table>';
-            $scope.Variables.htmlContent.dataSet = {
-                "dataValue": msg
-            };
-            $scope.Variables.order_for_status.dataSet = {
-                "dataValue": ''
-            };
             $scope.Widgets.OrdersContainer.show = false;
             $scope.Widgets.OrderStatusContainer.show = true;
             $scope.Widgets.OrderDetailContainer.show = false;
             $scope.Widgets.OrdersByProductContainer.show = false;
             $scope.Widgets.OrdersByStoreContainer.show = false;
             $scope.Widgets.OrdersByUserContainer.show = false;
+            $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+            $scope.Widgets.OEByStoreContainer.show = false;
+            $scope.Widgets.OEByProductAndStoreContainer.show = false;
 
         };
 
@@ -137,97 +120,245 @@ Application.$controller("MainPageController", ["$scope", function($scope) {
 
     $scope.svcOrderDetailonResult = function(variable, data) {
         if (data.results.ORDER_NBR != '') {
-            var msg = '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2"><tbody>';
-            msg += '<tr><td><b>Order Number:</b></td><td colspan="3">' + data.results.ORDER_NBR + '</td></tr>';
-            msg += '<tr><td><b>Item</b></td><td><b>Order Qty</b></td><td><b>Modify Qty</b></td><td><b>Ship Qty</b></td></tr>';
-            data.results.ORDER_DETAIL.forEach(function(order, index) {
-                msg += '<tr><td>' + order.ITEM_DSC + '</td><td>' + order.ORDER_QTY + '</td><td>' + order.MODIFY_QTY + '</td><td>' + order.SHIP_QTY + '</td></tr>';
-            });
-            msg += '</tbody></table>';
-            $scope.Variables.htmlContent.dataSet = {
-                "dataValue": msg
-            };
-            $scope.Variables.order_for_status.dataSet = {
-                "dataValue": ''
-            };
             $scope.Widgets.OrdersContainer.show = false;
             $scope.Widgets.OrderStatusContainer.show = false;
             $scope.Widgets.OrderDetailContainer.show = true;
             $scope.Widgets.OrdersByProductContainer.show = false;
             $scope.Widgets.OrdersByStoreContainer.show = false;
             $scope.Widgets.OrdersByUserContainer.show = false;
+            $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+            $scope.Widgets.OEByStoreContainer.show = false;
+            $scope.Widgets.OEByProductAndStoreContainer.show = false;
         };
     };
 
 
     $scope.svcOrdersonResult = function(variable, data) {
         if ($scope.Variables.forceOrdersInvoke.dataSet.dataValue > 0) {
-            console.log('counter: ', $scope.Variables.forceOrdersInvoke.dataSet.dataValue);
-            var msg = '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2"><tbody>';
-            msg += '<tr><td><b>Store</b></td><td><b>Order Number</b></td><td><b>Order Date</b></td><td><b>Whse</b></td></tr>';
-            data.results.ORDERS.forEach(function(order, index) {
-                msg += '<tr><td>' + order.DEST_FACILITY_ID + '</td><td>' + order.ORDER_NBR + '</td><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td></tr>';
-            });
-            msg += '</tbody></table>';
-            $scope.Variables.htmlContent.dataSet = {
-                "dataValue": msg
-            };
             $scope.Widgets.OrdersContainer.show = true;
             $scope.Widgets.OrderStatusContainer.show = false;
             $scope.Widgets.OrderDetailContainer.show = false;
             $scope.Widgets.OrdersByProductContainer.show = false;
             $scope.Widgets.OrdersByStoreContainer.show = false;
             $scope.Widgets.OrdersByUserContainer.show = false;
+            $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+            $scope.Widgets.OEByStoreContainer.show = false;
+            $scope.Widgets.OEByProductAndStoreContainer.show = false;
         };
     };
 
     $scope.svcOrdersByUseronResult = function(variable, data) {
         if (data.results.DW_CREATE_USER_ID != '') {
-            var msg = '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2"><tbody>';
-            msg += '<tr><td><b>User:</b></td><td colspan="3">' + data.results.DW_CREATE_USER_ID + '</td></tr>';
-            msg += '<tr><td><b>Order Nbr</b></td><td><b>Order Date</b></td><td><b>Whse</b></td><td><b>Store</b></td></tr>';
-            data.results.ORDERS.forEach(function(order, index) {
-                msg += '<tr><td>' + order.ORDER_NBR + '</td><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td><td>' + order.DEST_FACILITY_ID + '</td></tr>';
-            });
-            msg += '</tbody></table>';
-            $scope.Variables.htmlContent.dataSet = {
-                "dataValue": msg
-            };
-            $scope.Variables.user.dataSet = {
-                "dataValue": ''
-            };
             $scope.Widgets.OrdersContainer.show = false;
             $scope.Widgets.OrderStatusContainer.show = false;
             $scope.Widgets.OrderDetailContainer.show = false;
             $scope.Widgets.OrdersByProductContainer.show = false;
             $scope.Widgets.OrdersByStoreContainer.show = false;
             $scope.Widgets.OrdersByUserContainer.show = true;
+            $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+            $scope.Widgets.OEByStoreContainer.show = false;
+            $scope.Widgets.OEByProductAndStoreContainer.show = false;
         };
     };
 
 
     $scope.svcOrdersByStoreonResult = function(variable, data) {
         if (data.results.DEST_FACILITY_ID != '') {
-            var msg = '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2"><tbody>';
-            msg += '<tr><td><b>Store:</b></td><td colspan="2">' + data.results.DEST_FACILITY_ID + '</td></tr>';
-            msg += '<tr><td><b>Order Nbr</b></td><td><b>Order Date</b></td><td><b>Whse</b></td></tr>';
-            data.results.ORDERS.forEach(function(order, index) {
-                msg += '<tr><td>' + order.ORDER_NBR + '</td><td>' + order.ORDER_DT + '</td><td>' + order.SHIP_FACILITY_ID + '</td></tr>';
-            });
-            msg += '</tbody></table>';
-            $scope.Variables.htmlContent.dataSet = {
-                "dataValue": msg
-            };
-            $scope.Variables.store.dataSet = {
-                "dataValue": ''
-            };
             $scope.Widgets.OrdersContainer.show = false;
             $scope.Widgets.OrderStatusContainer.show = false;
             $scope.Widgets.OrderDetailContainer.show = false;
             $scope.Widgets.OrdersByProductContainer.show = false;
             $scope.Widgets.OrdersByStoreContainer.show = true;
             $scope.Widgets.OrdersByUserContainer.show = false;
+            $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+            $scope.Widgets.OEByStoreContainer.show = false;
+            $scope.Widgets.OEByProductAndStoreContainer.show = false;
         };
+    };
+
+
+    $scope.svcOrdersByProductAndStoreonResult = function(variable, data) {
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = true;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+    };
+
+
+    $scope.svcOrderExceptionsByStoreonResult = function(variable, data) {
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = true;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+    };
+
+
+    $scope.svcOrderExceptionsByProductAndStoreonResult = function(variable, data) {
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = true;
+    };
+
+
+    $scope.text8Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.product.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = true;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+    };
+
+
+    $scope.text9Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.store.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = true;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+    };
+
+
+    $scope.text10Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.user.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = true;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+    };
+
+
+    $scope.text11Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.product_for_product_and_store.dataSet = {
+            "dataValue": newVal
+        };
+    };
+
+
+    $scope.text13Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.store_for_product_and_store.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = true;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+
+    };
+
+
+    $scope.text14Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.order_for_detail.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = true;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+
+    };
+
+
+    $scope.text15Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.order_for_status.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = true;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+
+    };
+
+
+    $scope.text16Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.store_for_exceptions_by_store.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = true;
+        $scope.Widgets.OEByProductAndStoreContainer.show = false;
+
+    };
+
+
+    $scope.text17Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.product_for_exceptions_by_product_and_store.dataSet = {
+            "dataValue": newVal
+        };
+    };
+
+
+    $scope.text18Change = function($event, $isolateScope, newVal, oldVal) {
+        $scope.Variables.store_for_exceptions_by_product_and_store.dataSet = {
+            "dataValue": newVal
+        };
+        $scope.Widgets.OrdersContainer.show = false;
+        $scope.Widgets.OrderStatusContainer.show = false;
+        $scope.Widgets.OrderDetailContainer.show = false;
+        $scope.Widgets.OrdersByProductContainer.show = false;
+        $scope.Widgets.OrdersByStoreContainer.show = false;
+        $scope.Widgets.OrdersByUserContainer.show = false;
+        $scope.Widgets.OrdersByProductAndStoreContainer.show = false;
+        $scope.Widgets.OEByStoreContainer.show = false;
+        $scope.Widgets.OEByProductAndStoreContainer.show = true;
     };
 
 }]);
@@ -268,6 +399,27 @@ Application.$controller("grid5Controller", ["$scope",
 ]);
 
 Application.$controller("grid6Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("grid7Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("grid8Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("grid9Controller", ["$scope",
     function($scope) {
         "use strict";
         $scope.ctrlScope = $scope;
